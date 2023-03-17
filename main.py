7f0f29923045af281de530a9a4c5fc9dbe4d5f98
Wouter Groenewold <w.groenewold@rug.nl>
2023-03-17 09:29:08 +0100

    removed binary since pigeons block port 2306

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from typing import Union
+# from typing import Union
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
+import smtplib
+
+server = smtplib.SMTP('localhost')
+server.set_debuglevel(1)
+server.sendmail("From: wgroenewold@gmail.com", "To: wgroenewold@gmail.com", "Hoera, dit werkt")
+server.quit();
--- readme.md
+++ readme.md
@@ -15,11 +15,6 @@ git clone git@github.com:wgroenewold/listsrv-api.git
 ```
 
 ## Notes
-- Binary for lcmdx is a patched version, found in bin/src. Original version can be found [here](https://ftp.lsoft.com/CONTRIB/lcmdx.c), you can compile it yourself with
-
-```shell
-sudo gcc -O lcmdx.c -o lcmdx
-```
 
 
 ## Todo
